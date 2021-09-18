1caa1da3a629b28b8a5e4b6187d7b55b7113c772
Héctor Arranz <32869700+hectarra@users.noreply.github.com>
2021-09-19 01:20:33 +0200

    fixing client errors

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
   webpackDevMiddleware: config => {
     config.watchOptions.poll = 300;
     return config;
